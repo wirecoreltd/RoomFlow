3aa83f9231958a6265f0392cebbb5917a33cb121
wirecoreltd <wirecoreltd@gmail.com>
2026-07-17 09:37:35 +0400

    Create loading.tsx

---
+++ src/app/(app)/admin/resources/loading.tsx
@@ -0,0 +1,9 @@
+// src/app/(app)/admin/resources/loading.tsx
+export default function Loading() {
+  return (
+    <div className="space-y-4 animate-pulse">
+      <div className="h-6 w-48 bg-black/10 rounded" />
+      <div className="h-40 bg-black/5 rounded-lg" />
+    </div>
+  );
+}
