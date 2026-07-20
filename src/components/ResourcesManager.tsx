49b5002731fc5c9e987f11fb10a121b9b0f0e40f
wirecoreltd <wirecoreltd@gmail.com>
2026-07-20 09:48:04 +0400

    Update ResourcesManager.tsx

--- src/components/ResourcesManager.tsx
+++ src/components/ResourcesManager.tsx
@@ -423,7 +423,7 @@ export default function ResourcesManager({
   }
 
   return (
-    <div className="p-6 sm:p-8 space-y-8 max-w-6xl mx-auto">
+    <div className="space-y-8">
       {/* En-tête */}
       <div className="flex flex-wrap items-start justify-between gap-4">
         <div>
